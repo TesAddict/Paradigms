eb7e80ef64849eff7abf33fcd7dbaa2acad53de7
Methos <lefty.amperiadis@outlook.com>
2019-04-06 17:55:29 -0400

    started homework 6

--- Homework/4/Homework4.scm
+++ Homework/4/Homework4.scm
@@ -79,6 +79,8 @@
 ; 4.  Write a function but-last that takes two arguments, lst and num, and which returns the
 ; list of all but the last num elements of lst.
 
+;; This question is essentially the reverse of question 3. 
+
 ; 5.  Write a function end that takes two arguments, lst and num, and returns the last num
 ; elements of lst.
 
